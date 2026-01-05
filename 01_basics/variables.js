04a8b422e43c987d596d5c97a17d80abe2a30a51
parida1812 <158996743+parida1812@users.noreply.github.com>
2026-01-05 14:59:32 +0000

    learn about variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let accountHolder = "Debasish"
+let accountEmail = "deb@gmail.com"
+var address = "juania"
+designation = "SE"
+let accountState;
+
+// accountId = 14785
+// console.log(accountId);
+
+accountEmail = "point@gmail.com"
+accountHolder = "lion"
+console.table([accountEmail, accountHolder, accountId,designation, accountState])

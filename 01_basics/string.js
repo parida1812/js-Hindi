110e20621f3726980b70e4104f04452a181c899f
parida1812 <158996743+parida1812@users.noreply.github.com>
2026-01-17 10:39:36 +0000

    datatypes

---
+++ 01_basics/string.js
@@ -0,0 +1,11 @@
+let gameName = "Hitesh"
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.charAt(5));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('i'));
+console.log(gameName.substring(0,2));
+console.log(gameName.substring(1,6));
+
+
---
+++ datatypes_summary.js
@@ -0,0 +1,6 @@
+const id = 3.5
+const decId = 3.5
+console.log(id === decId);
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(Id === anotherId);
